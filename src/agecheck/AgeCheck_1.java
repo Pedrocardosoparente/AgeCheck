67d02f64d5b597959839181843250a4b745bbb78
Aluno <Aluno@10.85.198.36>
2024-06-05 16:20:04 -0300

    age check

---
+++ src/agecheck/AgeCheck_1.java
@@ -0,0 +1,34 @@
+public class AgeCheck_1 {
+   
+    public static void main(String[] args) {
+        int myAge = 19;   // I am 19; let me see if I can drive yet
+        
+        if (myAge >= 16) {
+            System.out.println("I'm old enough to have a driver's license!");
+        }
+        if (myAge <= 16) {
+            System.out.println("I'm not old enough yet... :*(");
+        }
+    }
+}
+    
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+/**
+ *
+ * @author Aluno
+ */
+
+
+    /**
+     * @param args the command line arguments
+     */
+
+        // TODO code application logic here
+    
+    
